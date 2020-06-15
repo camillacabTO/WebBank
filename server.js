03f4dbea8d2b537d97119ee70beaa1264b79fcfb
Camila Barros <camillacab@hotmail.com>
2020-06-15 16:45:20 -0400

    final #1 before review

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const express = require('express');
 const Handlebars = require("handlebars");   
 const bodyParser = require('body-parser');
 const expbs = require('express-handlebars');
+const expressValidator = require("express-validator");
 const data = require('./users');
 const app = express();
 
 let activeUser = '';
+
+// CREATE PLACEHOLDERS FOR TEXTFIELDS!!!!
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -23,29 +26,41 @@ app.use(express.static(path.join(__dirname, 'public')));
 const users = data.readData();
 
 Handlebars.registerHelper('createAccountNumber', function () {
-    return Math.floor(Math.random() * 10000000);
+    return Math.floor(Math.random() * 10000000); // generate random account number 
 });
 
+app.use(expressValidator());
+
 app.post('/register', (req, res) => {
+
+    req.check("email", "Invalid email address").isEmail();							 
+	req.check("password", "Password must contain 8 or more characters").isLength({min: 8});
+    req.check("password", "Mismatched Password and Confirm Password").equals(req.body.passwordConfirmation);
+
+    var errorArray = req.validationErrors();
+
     let found = users.find((user) => user.email === req.body.email);
-    console.log(`${req.body.email} ${req.body.password}`);
-    if (!found) {
-        users.push({ email: req.body.email, password: req.body.password});
-        res.render('index', { didRegistered: true });
+    console.log(`${req.body.email} ${req.body.password} ${req.body.passwordConfirmation}`);
+    if (!found) { // failure
+        if (errorArray) {
+            res.render('register' , { errors: errorArray });
+        } else {
+            users.push({ email: req.body.email, password: req.body.password});
+            res.render('index', { didRegistered: true });
+        }
     } else {
-        res.render('register', { doesExist: true });
+        res.render('register', { doesExist: true }); // success
     }
     data.writeData(users);
+
 });
 
 app.get('/login', (req, res) => {
-    // res.json(users);
     res.render('index');
 });
 
 app.post('/login', (req, res) => {
     let found = users.find((user) => user.email === req.body.email);
-    // console.log(`${found.email} ${found.password}`);
     if (found) {
         if (found.password === req.body.password) {
             activeUser = req.body.email;
@@ -63,31 +78,23 @@ app.post('/login', (req, res) => {
 });
 
 app.get('/register', (req, res) => {
-    // res.json(users);
     res.render('register');
 });
 
 app.post('/openaccount', (req, res) => {
-    // res.send(`You have selected: ${req.body.operation}`);
     console.log(`${req.body.operation}`);
     res.render('openaccount');
 });
 
-app.post('/accountcreated', (req, res) => {
+app.post('/main', (req, res) => {
     let accountType = req.body.typeofaccount;
     console.log(accountType);
-    if (accountType === 'checking') {
-        res.render('bankMain', { 
-            accountCreated: true,
-            accountType: 'Checking',
-            activeUser: activeUser
-        });
-    } else {
-        res.render('bankMain', { 
-            accountCreated: true,
-            accountType: 'Savings'
-        });
-    }
+
+    res.render('bankMain', { 
+    accountCreated: true,
+    activeUser: activeUser,
+    accountType: (accountType === 'checking') ? 'Checking' : 'Savings'
+    });
 });
 
 app.listen(5000);
